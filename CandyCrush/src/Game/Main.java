faa45560a12d3183083e7406d10f83a175e75791
Jean K <jcsantoyac@correo.udistrital.edu.co>
2020-05-23 18:05:59 -0500

    Se ha creado el proyecto con propuesta de simbolos en el main

---
+++ CandyCrush/src/Game/Main.java
@@ -0,0 +1,15 @@
+package Game;
+
+/*
+    Este programa tiene el propósito de emular el juego Candy Crush por consola 
+    Última edición 23/may/2020
+*/
+
+//propuestas simbolos dulces ★ ☆ ✮ ✯ ✦ ✧ ☀ ♢ ๑ ★ ☆ ▣ ▤ ▥ ▦ ▩ ☼ ⊙ ◎ ◁ ▷ ✙ ☸ ◈ ◇ ✠ ✡ ✢ ✣ ✤ ✥ ✦ ✧ ★ ✩ ✪ ✫ 
+//                           ✬ ✭ ✮ ✯ ✰ ✱ ✲ ✳ ✴ ✶ ✷ ✸ ✹ ✺ ✻ ✼ ✽ ✾ ❀ ❁ ❂ ❃ ❄ ❅ ❆ ❇ ❈ ❉ ❊ ❋
+
+public class Main {
+    public static void main(String args[]){
+        
+    }
+}

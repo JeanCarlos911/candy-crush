358259550f14b520efbb32d90f9dfa5e18fae43e
spaezsuarez2 <lamperougelelouch74@gmail.com>
2020-06-01 18:45:52 -0500

    Estructura inicial del proyecto

--- CandyCrush/src/Main.java
+++ CandyCrush/src/Main.java
@@ -1,8 +1,6 @@
-package Game;
 
 /*
     Este programa tiene el propósito de emular el juego Candy Crush por consola
-    Última edición 23/may/2020
 */
 
 //propuestas simbolos dulces ★ ☆ ✮ ✯ ✦ ✧ ☀ ♢ ๑ ★ ☆ ▣ ▤ ▥ ▦ ▩ ☼ ⊙ ◎ ◁ ▷ ✙ ☸ ◈ ◇ ✠ ✡ ✢ ✣ ✤ ✥ ✦ ✧ ★ ✩ ✪ ✫
@@ -10,7 +8,7 @@ package Game;
 
 public class Main {
     public static void main(String args[]){
-
-        //configurando commit
+       
+        
     }
 }

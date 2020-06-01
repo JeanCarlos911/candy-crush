72d786be5e5a92092cbaf55223fbbfbc2c4d84fa
Camilo74 <joalcamiloparraguio@gmail.com>
2020-06-01 08:25:10 -0500

    Probando commit

--- CandyCrush/src/Game/Main.java
+++ CandyCrush/src/Game/Main.java
@@ -1,15 +1,16 @@
 package Game;
 
 /*
-    Este programa tiene el propósito de emular el juego Candy Crush por consola 
+    Este programa tiene el propósito de emular el juego Candy Crush por consola
     Última edición 23/may/2020
 */
 
-//propuestas simbolos dulces ★ ☆ ✮ ✯ ✦ ✧ ☀ ♢ ๑ ★ ☆ ▣ ▤ ▥ ▦ ▩ ☼ ⊙ ◎ ◁ ▷ ✙ ☸ ◈ ◇ ✠ ✡ ✢ ✣ ✤ ✥ ✦ ✧ ★ ✩ ✪ ✫ 
+//propuestas simbolos dulces ★ ☆ ✮ ✯ ✦ ✧ ☀ ♢ ๑ ★ ☆ ▣ ▤ ▥ ▦ ▩ ☼ ⊙ ◎ ◁ ▷ ✙ ☸ ◈ ◇ ✠ ✡ ✢ ✣ ✤ ✥ ✦ ✧ ★ ✩ ✪ ✫
 //                           ✬ ✭ ✮ ✯ ✰ ✱ ✲ ✳ ✴ ✶ ✷ ✸ ✹ ✺ ✻ ✼ ✽ ✾ ❀ ❁ ❂ ❃ ❄ ❅ ❆ ❇ ❈ ❉ ❊ ❋
 
 public class Main {
     public static void main(String args[]){
-        
+
+        //configurando commit
     }
 }

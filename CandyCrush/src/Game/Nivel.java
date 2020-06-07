4ebd18bc4ec1f299ecb4bcec3be9f49ff91b0c2d
JeanCarlos911 <jcsantoyac@correo.udistrital.edu.co>
2020-06-07 16:29:45 -0500

    added class Nivel.java

---
+++ CandyCrush/src/Game/Nivel.java
@@ -0,0 +1,40 @@
+package Game;
+
+public class Nivel {
+    //Atributos
+    private int nivel;
+    private String nombre;
+    private long puntuacionObjetivo;
+    
+    //creacion del Nivel
+    public Nivel(){
+        //Valores iniciales por defecto
+        nivel=1;
+        nombre="Jelly";
+        puntuacionObjetivo=1000;
+    }
+
+//Metodos propios del Nivel-----------------------------------------------
+    /**
+     * Este metodo por ahora solo cambia de nivel segun la puntuacion dada y
+     * devuelve un boolean que envia si hubo cambio de nivel (true) o no (false);
+     * @param puntuacion long puntuacion actual@
+     */
+    public boolean pasarNivel(long puntuacion){
+        if(puntuacion>=puntuacionObjetivo){
+            nivel+=1;
+            puntuacionObjetivo+=500;
+            if(nivel==2){
+                nombre="Ingredient";
+            }else if(nivel==3){
+                nombre="Timed";
+            }else{
+                nombre="Jelly extra";
+            }
+            return true;
+        }else{
+            return false;
+        }
+    }
+    
+}

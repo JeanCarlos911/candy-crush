147a49b18cf048797f5346e4ab9a3cfeba00a240
JeanCarlos911 <jcsantoyac@correo.udistrital.edu.co>
2020-06-07 16:59:18 -0500

    added metods iniciar() & Menu in class Menu

--- CandyCrush/src/GUI/Menu.java
+++ CandyCrush/src/GUI/Menu.java
@@ -2,4 +2,17 @@ package GUI;
 
 public class Menu {
     
+    /**
+     * aqui se genera el menu cuando se crea
+     */
+    public Menu(){
+        //en proceso de construccion
+    }
+    
+    /**
+     * aqui se inicia un nuevo menu
+     */
+    public static void iniciar(){
+        Menu main = new Menu();
+    }
 }

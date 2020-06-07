b15f9fa0f37738f23af4f62e117a9f281fad3d46
JeanCarlos911 <jcsantoyac@correo.udistrital.edu.co>
2020-06-07 17:22:08 -0500

    added formaRamdon, added javadoc, updated metod Dulce()

--- CandyCrush/src/Game/Dulce.java
+++ CandyCrush/src/Game/Dulce.java
@@ -1,19 +1,32 @@
 package Game;
 
 public class Dulce {
-
-    private String forma;
-
-    public Dulce(){
-        this.forma = "";
-    }
-
-    public Dulce(String forma)
-    {
+    //Atributo
+    private int forma;
+    
+    /**
+     * Establece forma inicial del dulce al crearse
+     * @param forma es un int representativo de la forma
+     */
+    public Dulce(int forma){
         this.forma = forma;
     }
-
-    public String getForma(){
+    
+    /**
+     * Devuelve una forma aleatoria a traves de un int representativo
+     * @return int representativo
+     */
+    public static int formaRamdon(){
+        int numeroAleatorio = (int)(Math.random()*6+1);
+        return numeroAleatorio;
+    }
+    
+    /**
+     * Retorna forma del dulce
+     * @return int representativo de la forma
+     */
+    public int getForma(){
         return this.forma;
     }
+    
 }

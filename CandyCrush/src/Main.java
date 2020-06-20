11aac78cc5fd8e1ee883bd1d75dcd1e454941799
Jean_K <64989232+JeanCarlos911@users.noreply.github.com>
2020-06-20 16:13:43 -0500

    Interfaz básica Menu mejorada y adaptada a repl
    * updated gitignore
    
    * modified propuestas simbolos and added javadoc
    
    * test0 in repl
    
    * revision en repl completada
    
    * updated gitignore
    
    Co-authored-by: JeanCarlos911 <jcsantoyac@correo.udistrital.edu.co>

--- CandyCrush/src/Main.java
+++ CandyCrush/src/Main.java
@@ -1,9 +1,13 @@
 import GUI.Menu;
 
-//propuestas simbolos dulces ★ ☆ ✮ ✯ ✦ ✧ ☀ ♢ ๑ ★ ☆ ▣ ▤ ▥ ▦ ▩ ☼ ⊙ ◎ ◁ ▷ ✙ ☸ ◈ ◇ ✠ ✡ ✢ ✣ ✤ ✥ ✦ ✧ ★ ✩ ✪ ✫
-//                           ✬ ✭ ✮ ✯ ✰ ✱ ✲ ✳ ✴ ✶ ✷ ✸ ✹ ✺ ✻ ✼ ✽ ✾ ❀ ❁ ❂ ❃ ❄ ❅ ❆ ❇ ❈ ❉ ❊ ❋
+//propuestas simbolos dulces ★ ✦ ◈ ✴ ❄
 
-public class Main {
+public class Main{
+    
+    /**
+     * Main method
+     * @param args 
+     */
     public static void main(String args[]){
        Menu.iniciar();
     }

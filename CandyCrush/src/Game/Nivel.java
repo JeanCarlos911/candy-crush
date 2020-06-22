73767a1dfca57034e6f71383a53c3ece77da6f72
Jean Carlos Santoya Cabrera <jeancarlosodi@gmail.com>
2020-06-22 11:23:27 -0500

    se mejoro legibilidad y logica

--- CandyCrush/src/Game/Nivel.java
+++ CandyCrush/src/Game/Nivel.java
@@ -8,50 +8,25 @@ public class Nivel {
     
     //creacion del Nivel
     public Nivel(){
-        
         this.nivel = 1;
         this.nombre = "Jelly";
         this.puntuacionObjetivo = 1000;
     }
     
-    //Setters y getters
+    /**
+     * Retorna nombre del nivel
+     * @return string nombre del nivel actual
+     */
     public String getNombre(){
         return this.nombre;
     }
     
+    /**
+     * Retorna puntuacion objetivo del nivel actual
+     * @return long puntuacion objetivo
+     */
     public long getPuntuacionObjetivo(){
         return this.puntuacionObjetivo;
     }
-    
-    /**
-     * Este metodo por ahora solo cambia de nivel segun la puntuacion dada y
-     * devuelve un boolean que envia si hubo cambio de nivel (true) o no (false);
-     * @param puntuacion long puntuacion actual@
-     */
-    public boolean pasarNivel(long puntuacion){ 
 
-        if(puntuacion>=puntuacionObjetivo){
-
-            this.nivel += 1;
-            this.puntuacionObjetivo += 500;
-
-            if(this.nivel == 2){
-
-                this.nombre = "Ingredient";
-
-            }else if( this.nivel == 3){
-
-                this.nombre = "Timed";
-
-            }else{
-                this.nombre = "Jelly extra";
-            }
-
-            return true;
-            
-        }else{
-            return false;
-        }
-    }
-    
 }
--- CandyCrush/src/Game/Tablero.java
+++ CandyCrush/src/Game/Tablero.java
@@ -4,6 +4,7 @@ public class Tablero {
     //Atributos
     private Dulce matriz[][];
     private long puntuacion;
+    
     /**
      * Al crear un tablero, se establece el tamaño de la matriz de dulces
      */
@@ -11,45 +12,46 @@ public class Tablero {
         this.matriz = new Dulce[9][9];
     }
 
+    /**
+     * Establece dulce en la posicion dada del tablero
+     * @param fila fila del dulce a establecer
+     * @param columna columna del dulce a establecer
+     * @param dulce dulce que establecerá en la posicion dada
+     */
     public void setDulce(int fila,int columna,Dulce dulce){
         this.matriz[fila][columna] = dulce;
     }
-
+    
+    /**
+     * Obtiene dulce en la posición dada
+     * @param fila fila del dulce requerido
+     * @param columna columna del dulce requerido
+     * @return dulce requerido
+     */
     public Dulce getDulce(int fila,int columna){
         return this.matriz[fila][columna];
     }
 
+    /**
+     * Establece puntuacion segun parametro de entrada
+     * @param puntuacion long puntuacion a establecer
+     */
     public void setPuntuacion(long puntuacion){
         this.puntuacion = puntuacion;
     }
 
+    /**
+     * retorna puntuacion generada
+     * @return long puntuacion generada
+     */
     public long getPuntuacion(){
         return this.puntuacion;
     } 
     
-    public void actualizarTablero(){
-        
-        while(this.verificarTablero()){
-
-            //this.eliminarDulces();
-            //this.validarPuntuacion();
-            this.caerDulces();
-            this.llenarDulces();
-        }
-    }
-
-    public void organizar(){
-        
-        while(this.verificarTablero()){
-
-            //this.eliminarDulces();
-            this.caerDulces();
-            this.llenarDulces();
-        }
-    }
-
+    /**
+     * Define cada posicion de la matriz de dulces de manera aleatorea
+     */
     public void generarTablero(){
-        
         for(int i = 0; i < 9; i++){
             for(int j = 0; j < 9; j++){
                 this.setDulce(i, j, new Dulce(Dulce.formaRamdon()));          
@@ -57,129 +59,246 @@ public class Tablero {
         }
     }
 
-    public void moverDulce(int[] arreglo){
-
-        Dulce cambio = this.getDulce(arreglo[2], arreglo[3]);
-        Dulce dulceAmover = this.getDulce(arreglo[0],arreglo[1]);
-
-        this.setDulce(arreglo[0], arreglo[1], cambio);
-        this.setDulce(arreglo[2], arreglo[3], dulceAmover);
-
+    /**
+     * Modifica el tablero de tal forma que no hayan filas ni columnas formadas al iniciar
+     */
+    public void organizar(){
+        boolean existe = this.hayLinea();
+        System.out.println("paso por aca 1");
+        if(existe){
+            this.eliminarDulces();
+            System.out.println("paso por aca 2");
+            this.caerDulces();
+            this.llenarDulces();
+            organizar();
+        }
     }
 
-    private void llenarDulces(){
+    /**
+     * Verifica si hay linea válida en el tablero
+     * @return boolean true si hay linea
+     */
+    private boolean hayLinea() {
+        int fila = 0, columna = 0, contadorDulces = 1;
         
+        while(fila < 9){
+            while(columna < 8){
+                if(this.matriz[fila][columna].getForma() == this.matriz[fila][columna+1].getForma()){
+                    contadorDulces ++;
+                    if(columna<7){
+                        columna ++;
+                    }else if(contadorDulces > 2){
+                        return true;
+                    }else{
+                        if(fila < 8){
+                            columna = 0;
+                            fila ++;
+                        }else{
+                            columna ++;
+                            if(fila > 7){
+                                break;
+                            }
+                        }
+                    }
+                }else{
+                    if(contadorDulces > 2){
+                        return true;
+                    }else{
+                        /*Si no ha formado la fila independientemente de si debe seguir recorriendola el contador
+                        de dulces se vuelve a resetear*/
+                        contadorDulces = 1;
+                        /*Condicion para mirar si esta en la penultima columna y debe pasar a la siguiente
+                        fila o se mantiene en la misma*/
+                        if(columna > 6 && fila < 8){
+                            columna = 0;
+                            fila ++;
+                        }else{
+                            columna ++;
+                            if(fila > 7){
+                                break;
+                            }
+                        }
+                    }   
+                }
+            }
+        }
+        fila = 0; columna = 0;contadorDulces = 1;
+        
+        while(columna < 9){
+            while(fila < 8){
+                System.out.println("bucle");
+                if(this.getDulce(fila, columna).getForma() == this.getDulce(fila+1, columna).getForma()){
+                    contadorDulces ++;
+                    fila += 1;
+                }else{
+                    if(contadorDulces > 2){
+                        return true;
+                    }else{
+                        contadorDulces = 1;
+                        if(fila > 6){
+                            fila = 0;
+                            columna ++;
+                        }else{
+                            fila ++;
+                            if(columna > 7){
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
+        return false;
+    }
+
+    /**
+     * Encuentra linea válida en el tablero
+     * @return int[] con descripciones de la linea encontrada
+     */
+    private int[] encontrarLinea() {
+        int fila = 0, columna = 0, contadorDulces = 1;
+        
+        while(fila < 9){
+            while(columna < 8){
+                if(this.getDulce(fila, columna).getForma() == this.getDulce(fila, columna+1).getForma()){
+                    contadorDulces ++;
+                    columna ++;
+                }else{
+                    if(contadorDulces > 2){
+                        return new int[]{contadorDulces, fila, columna + 1, 0};
+                    }else{
+                        contadorDulces = 1;
+                        if(columna > 6){
+                            columna = 0;
+                            fila ++;
+                        }else{
+                            columna ++;
+                        }
+                    }   
+                }
+            }
+        }
+        fila = 0; columna = 0; contadorDulces = 1;
+        
+        while(columna < 9){
+            while(fila < 8){
+                if(this.getDulce(fila, columna).getForma() == this.getDulce(fila + 1, columna).getForma()){
+                    contadorDulces ++;
+                    fila += 1;
+                }else{
+                    if(contadorDulces > 2){
+                        return new int[]{contadorDulces, fila + 1, columna, 1};
+                    }else{
+                        contadorDulces = 1;
+                        if(fila > 6){
+                            fila = 0;
+                            columna ++;
+                        }else{
+                            fila ++;
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("error, fila no encontrada");
+        return new int[]{-1};
+    }
+
+    /**
+     * Este metodo elimina una fila de dulces encontrados (transforma su forma a vacio es decir a int 0)
+     */
+    private void eliminarDulces() {
+        int[] dato = encontrarLinea();
+        int fila = dato[1];
+        int columna = dato[2];
+        
+        for (int dulces = dato[0]; dulces > 0; dulces--){
+            this.matriz[fila][columna].setForma(0);
+            if(dato[3] == 0){
+                columna--;
+            }else {
+                fila--;
+            }
+        }
+    }
+
+    /**
+     * Se encarga de la gravedad característica del candy crush cuando se elimina algún dulces
+     */
+    private void caerDulces(){
         for(int i = 0; i < 8; i++){
-            for(int j = 0; i < 8; j++){
-                if(this.getDulce(i, j) == null){
-                    this.setDulce(i, j,new Dulce(Dulce.formaRamdon()));
+            for(int fila = 0; fila < 8; fila++){
+                for(int columna = 0; columna < 9; columna++){
+                    if(this.getDulce(fila, columna).getForma() != 0 && this.getDulce(fila+1, columna).getForma() == 0){
+                        this.matriz[fila+1][columna].setForma(matriz[fila][columna].getForma());
+                        this.matriz[fila][columna].setForma(0);
+                        this.llenarDulces();
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Reemplaza los espacios vacios por nuevos dulces
+     */
+    private void llenarDulces(){
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j < 9; j++){
+                if(this.matriz[i][j].getForma() == 0){
+                    this.matriz[i][j].setForma(Dulce.formaRamdon());
                 } 
             }
         }
     }
 
-    private void caerDulces(){
-        
-        for(int i = 0; i <= 7; i++){
-            for(int j = 0; i < 8; j++){
-                if(this.getDulce(i,j) != null && this.getDulce(i+1,j) == null){
-                    Dulce dulceCayo = this.getDulce(i,j);
-                    this.setDulce(i, j, null);
-                    this.setDulce(j+1, j, dulceCayo);
-                    this.llenarDulces();
+    /**
+     * Mueve dos dulces segun posiciones dadas por el jugador en lenguaje tablero
+     * @param dato arreglo con posiciones dadas por el usuario y traducidas a lenguaje tablero
+     */
+    public void moverDulce(int[] dato){
+        int formaA = this.getDulce(dato[0], dato[1]).getForma();
+        int formaB = this.getDulce(dato[2],dato[3]).getForma();
 
-                }
-                
-            }
-                
+        this.matriz[dato[0]][dato[1]].setForma(formaB);
+        this.matriz[dato[2]][dato[3]].setForma(formaA);
+    }
+
+    /**
+     * Actualiza el tablero con la variante que adiciona la puntuacion generada
+     */
+    public void actualizarTablero(){
+        boolean existe = this.hayLinea();
+        if(existe){
+            this.eliminarDulces();
+            this.validarPuntuacion();
+            this.caerDulces();
+            this.llenarDulces();
+            actualizarTablero();
         }
     }
 
-    private boolean hayFila(){
-
-        int fila = 0,columna = 0,contadorDulces = 1;
-        boolean filas = false;
-
-        while(fila < 8){
-            while(columna <= 7){
-                if(this.getDulce(fila, columna).getForma() == this.getDulce(fila, columna+1).getForma()){
-                    contadorDulces += 1;
-                    columna += 1;
-                }else{
-                    if(contadorDulces >= 3 && contadorDulces <= 6){
-                        filas = true;
-                        this.validarPuntuacion(contadorDulces);
-                        return filas;
-                    }else{
-                        /*Si no ha formado la fila indeendientemente de si debe seguir recorriendola
-                          El contador de dulces se vuelve a resetear
-                        */
-                        contadorDulces = 1;
-
-                        /*Condicion para mirar si esta en la penultima columna y debe pasar a la siguiente
-                        fila o se mantiene en la misma*/
-
-                        if(columna >= 7){
-                            columna = 0;
-                            fila += 1;
-                        }else{
-                            columna += 1;
-                        }
-                        
-                    }   
+    /**
+     * Adiciona la puntuacion generada por el movimiento del usuario
+     */
+    private void validarPuntuacion() {
+        int cantidadDulces = 0;
+        for(int i = 0; i < 9; i++){
+            for(int j = 0; j<9; j++){
+                if(this.matriz[i][j].getForma() == 0){
+                    cantidadDulces++;
                 }
             }
-            
         }
-        
-        return filas;
+        sumarPuntuacion(cantidadDulces);
     }
-
-    private boolean hayColumna(){
-
-        int fila = 0,columna = 0,contadorDulces = 1;
-        boolean columnas = false;
-
-        while(columna < 8){
-            while(fila <= 7){
-                if(this.getDulce(fila, columna).getForma() == this.getDulce(fila+1, columna).getForma()){
-                    contadorDulces += 1;
-                    fila += 1;
-                }else{
-                    if(contadorDulces >= 3 && contadorDulces <= 6){
-                        columnas = true;
-                        this.validarPuntuacion(contadorDulces);
-                        return columnas;
-                    }else{
-                        contadorDulces = 0;
-                        if(fila >= 7){
-                            fila = 0;
-                            columna += 1;
-                        }else{
-                            fila += 1;
-                        }
-                    }
-                    
-                }
-            }
-            
-        }
-        
-        return columnas;
-    }
-
-    private boolean verificarTablero() {
-        if(this.hayFila() || this.hayColumna()){
-            return true;
-        }
-        return false;
-    }
-
-    private void eliminarDulces() {
-        throw new UnsupportedOperationException("Not supported yet."); 
-    }
-
-    private void validarPuntuacion(int cantidadDulces) {
+    
+    /**
+     * Suma puntuacion dependiendo del numero de dulces en serie dados
+     * @param cantidadDulces numero de dulces en serie
+     */
+    private void sumarPuntuacion(int cantidadDulces){
         switch(cantidadDulces){
             case 3:
                 this.puntuacion += 50;
@@ -196,7 +315,6 @@ public class Tablero {
             default:
                 this.puntuacion += 0;
                 break;
-
         }
     }
     
